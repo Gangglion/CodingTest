0f18fdf39a5af44e3b90e2442b1043ed2384cb5f
SeungHyeob Han <o0tmdguq0o@gmail.com>
2022-08-20 22:46:54 +0900

    백준코드 레벨1
    백준코드 레벨1 1번부터 15번까지 코드

---
+++ Level1/src/Main.java
@@ -0,0 +1,177 @@
+//1번
+// public class Main{
+//     public static void main(String[] args) throws Exception{
+//         System.out.println("Hello World!");
+//     } 
+// }
+
+// 2번
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         System.out.println("강한친구 대한육군\n강한친구 대한육군");
+//     }
+// }
+
+//3,4,5번
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         int a,b=0;
+//         Scanner sc = new Scanner(System.in);
+//         a=sc.nextInt();
+//         b=sc.nextInt();
+//         System.out.println(a+b);
+//         System.out.println(a-b);
+//         System.out.println(a*b);
+//     }
+// }
+
+//6번
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         double a,b=0.0;
+//         Scanner sc = new Scanner(System.in);
+//         a=sc.nextDouble();
+//         b=sc.nextDouble();
+//         System.out.println(a/b);
+        
+//     }
+// }
+
+//7번
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         int a,b=0;
+//         Scanner sc = new Scanner(System.in);
+//         a=sc.nextInt();
+//         b=sc.nextInt();
+//         System.out.println(a+b);
+//         System.out.println(a-b);
+//         System.out.println(a*b);
+//         System.out.println(a/b);
+//         System.out.println(a%b);
+        
+//     }
+// }
+
+//8번
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         Scanner sc = new Scanner(System.in);
+//         String inputid = "";
+//         inputid = sc.next();
+//         System.out.println(inputid+"??!");
+//     }
+// }
+
+//9번
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         Scanner sc = new Scanner(System.in);
+//         int year = 0;
+//         year = sc.nextInt();
+//         System.out.println(year-543);
+//     }
+// }
+
+//10번
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         Scanner sc = new Scanner(System.in);
+//         int[] need = {1,1,2,2,2,8};
+//         int[] get = new int[6];
+//         for(int i=0;i<need.length;i++)
+//         {
+//             get[i]=sc.nextInt();
+//         }
+//         for(int i=0;i<need.length;i++)
+//         {
+//             System.out.printf("%d ",need[i]-get[i]);
+//         }
+//     }
+// }
+
+//11번
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         Scanner sc = new Scanner(System.in);
+//         int a,b,c=0;
+//         a=sc.nextInt();
+//         b=sc.nextInt();
+//         c=sc.nextInt();
+//         System.out.println((a+b)%c);
+//         System.out.println(((a%c)+(b%c))%c);
+//         System.out.println((a*b)%c);
+//         System.out.println(((a%c)*(b%c))%c);
+//     }
+// }
+
+//12번
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         Scanner sc = new Scanner(System.in);
+//         int a,b=0;
+//         a=sc.nextInt();
+//         b=sc.nextInt();
+//         System.out.printf("%d\n",(a*(b%10)));
+//         System.out.printf("%d\n",(a*((b%100)/10)));
+//         System.out.printf("%d\n",(a*(b/100)));
+//         System.out.println(a*b);
+//     }
+// }
+
+//13번
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         System.out.println("\\    /\\");
+//         System.out.println(" )  ( ')");
+//         System.out.println("(  /  )");
+//         System.out.println(" \\(__)|");
+//     }
+// }
+
+//14번
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         System.out.println("|\\_/|");
+//         System.out.println("|q p|   /}");
+//         System.out.println("( 0 )\"\"\"\\");
+//         System.out.println("|\"^\"`    |");
+//         System.out.println("||_/=\\\\__|");
+//     }
+// }
+
+//15번
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r\'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
+/*
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+*/
